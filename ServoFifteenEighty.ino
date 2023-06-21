d527e99c05128e2bf8e63da0fcc3267b8efa8d3a
Zach Garcia <36376800+ZachGarcia42@users.noreply.github.com>
2023-06-20 17:55:53 -0700

    Add files via upload

---
+++ ServoFifteenEighty.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+// int angle = 0;
+
+void setup() {
+  Serial.begin(9600);
+  // myservo.attach(12, 500, 2500);  // attaches the servo on pin 0 to the servo object
+  myservo.attach(12, 600, 2380);
+
+  // start at 15 degrees. Due to servo orientation, writeAngle = 90 - desiredAngle.
+  myservo.write(90-15);
+
+  //wait one second before starting to raise
+  delay(1000);
+
+  for (pos = 15; pos <= 80; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Serial.println(pos);
+    myservo.write(90-pos);              // tell servo to go to position in variable 'pos'
+    delay(123);                       // waits 123 ms for the servo to reach the position. This should work out to be 8 seconds from 15 degrees to 80 degrees
+  }
+}
+
+void loop() {}
